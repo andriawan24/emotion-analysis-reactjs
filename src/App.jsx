e69b004022d03b8c160a2da33e595ed09b7a05db
Naufal Fawwaz <fawaznaufal23@gmail.com>
2022-09-11 20:08:17 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable no-console */
+import {
+  Button, Container, Heading, Image, Input,
+} from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function App() {
+  const [imageURL, setImageURL] = useState();
+  const [preview, setPreview] = useState();
+
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    if (!imageURL) {
+      setPreview(undefined);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(imageURL);
+    setPreview(objectUrl);
+  }, [imageURL]);
+
+  const handleFileChange = (e) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setImageURL(undefined);
+      return;
+    }
+
+    // I've kept this example simple by using the first image instead of multiple
+    setImageURL(e.target.files[0]);
+  };
+
+  const handleSubmit = () => {
+    navigator('/result');
+  };
+
+  return (
+    <Container maxW="container.lg" py={20}>
+      <Heading fontSize="xl">Pilih gambar terlebih dahulu</Heading>
+      {imageURL && <Image mt={6} src={preview} alt="chosen" w="full" h={300} fit="fill" />}
+      <Input type="file" onChange={handleFileChange} accept="image/*" mt={10} />
+      <Button w="full" mt={10} onClick={handleSubmit}>
+        Analyze
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/Result.jsx
@@ -0,0 +1,44 @@
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import {
+  Container, Flex, Heading, Table, TableContainer, Tbody, Td, Th, Thead, Tr,
+} from '@chakra-ui/react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Result() {
+  const navigate = useNavigate();
+
+  const handleBackButton = () => {
+    navigate(-1);
+  };
+
+  return (
+    <Container maxW="container.lg" py={20}>
+      <Flex alignItems="center">
+        <ArrowBackIcon fontWeight="bold" fontSize="xl" mr={4} onClick={handleBackButton} _hover={{ cursor: 'pointer' }} />
+        {' '}
+        <Heading fontSize="xl">Hasil Analisis</Heading>
+      </Flex>
+      <TableContainer mt={10}>
+        <Table variant="striped">
+          <Thead>
+            <Tr>
+              <Th>Nomor</Th>
+              <Th>Nama Anak</Th>
+              <Th>Emosi</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>1</Td>
+              <Td>Jajang</Td>
+              <Td>Marah (20%)</Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
+}
+
+export default Result;
